9888a0d1d0c973ff23daf4eea9dfbc19f69aef2f
Matheus Ryuki <matheuskaiya2@gmail.com>
2024-03-12 14:57:06 -0300

    Personalização d o estilo do componente de coordenadas do mouse na rota '/mapa'

--- resources/js/Pages/Mapa/Functions/InicializarMapa.js
+++ resources/js/Pages/Mapa/Functions/InicializarMapa.js
@@ -1,4 +1,4 @@
-import { ZoomInOut } from './Zoom/ZoomInOut.js';
+import { ZoomInOut } from "./Zoom/ZoomInOut.js";
 
 let logMessages = false;
 
@@ -18,35 +18,43 @@ function AdicionaCoordenadasMouse(map, configuracoesLeaflet) {
     let proj4text = configuracoesLeaflet.proj4text;
     let projCRS = new L.Proj.CRS(crs, proj4text);
     let mousePosControl = L.control.mousePosition({
-        position: "bottomright",
+        position: "bottomleft",
         emptyString: "Coordenadas indisponíveis",
         formatter: function (lng, lat) {
             let pt = projCRS.project(L.latLng(lat, lng));
-            if (sistema == 'utm') {
+            if (sistema == "utm") {
                 return "" + pt.y.toFixed(0) + " N : " + pt.x.toFixed(0) + " E";
             }
-            return "Lat.:" + pt.y.toFixed(5) + " | Lon.:" + pt.x.toFixed(5) + "";
-        }
+            return (
+                "Lat.:" + pt.y.toFixed(5) + " | Lon.:" + pt.x.toFixed(5) + ""
+            );
+        },
     });
     map.addControl(mousePosControl);
-    logMessages && console.log("   [CreateMap] Coordenadas do mouse adicionada ao mapa.");
+    logMessages &&
+        console.log("   [CreateMap] Coordenadas do mouse adicionada ao mapa.");
 }
 
-function AdicionaEscala(map, largura=150, posicao="bottomleft") {
-    let escala = L.control.scale({
-        position: posicao,
-        metric: true,
-        imperial: false,
-        maxWidth: largura,
-    }).addTo(map);
+function AdicionaEscala(map, largura = 150, posicao = "bottomleft") {
+    let escala = L.control
+        .scale({
+            position: posicao,
+            metric: true,
+            imperial: false,
+            maxWidth: largura,
+        })
+        .addTo(map);
     logMessages && console.log("   [CreateMap] Escala adicionada ao mapa.");
 }
 
 function FuncaoVisaoInicial(map, x, y, z) {
     window.SetaVisaoInicial = function () {
-        return map.setView([y,x], z);
+        return map.setView([y, x], z);
     };
-    logMessages && console.log("   [CreateMap] Função de resetar para visão inicial adicionada.");
+    logMessages &&
+        console.log(
+            "   [CreateMap] Função de resetar para visão inicial adicionada."
+        );
 }
 
 function FuncoesZoom(map) {
@@ -55,7 +63,8 @@ function FuncoesZoom(map) {
 
 function AdicionaAttribution(map, textoAttribution) {
     map.attributionControl.setPrefix(textoAttribution);
-    logMessages && console.log("   [CreateMap] Attribution adicionado ao mapa.");
+    logMessages &&
+        console.log("   [CreateMap] Attribution adicionado ao mapa.");
 }
 
 function CriaMenuContexto(map, configuracoesLeaflet) {
@@ -66,13 +75,24 @@ function CriaMenuContexto(map, configuracoesLeaflet) {
     let popup = L.popup();
     map.on("contextmenu", (e) => {
         let coordenada = projCRS.project(L.latLng(e.latlng.lat, e.latlng.lng));
-        if (sistema == 'utm') {
-            let content = "<b>N</b>: " + coordenada.y.toFixed(0) + "<br><b>E</b>: " + coordenada.x.toFixed(0) + "<br>";
+        if (sistema == "utm") {
+            let content =
+                "<b>N</b>: " +
+                coordenada.y.toFixed(0) +
+                "<br><b>E</b>: " +
+                coordenada.x.toFixed(0) +
+                "<br>";
         }
-        let content = "<b>Lat.</b>: " + coordenada.y.toFixed(5) + " <br><b>Lon.</b>: " + coordenada.x.toFixed(5) + "<br>";
+        let content =
+            "<b>Lat.</b>: " +
+            coordenada.y.toFixed(5) +
+            " <br><b>Lon.</b>: " +
+            coordenada.x.toFixed(5) +
+            "<br>";
         popup.setLatLng(e.latlng).setContent(content).openOn(map);
     });
-    logMessages && console.log("   [CreateMap] Menu de contexto adicionado ao mapa.");
+    logMessages &&
+        console.log("   [CreateMap] Menu de contexto adicionado ao mapa.");
 }
 
 function FuncaoMapaInformacoes(mapa) {
@@ -81,27 +101,31 @@ function FuncaoMapaInformacoes(mapa) {
         let centro = mapa.getCenter();
         let bounds = mapa.getBounds();
         let informacoes = {
-            zoom:zoom,
-            centro:centro,
-            bounds:bounds,
+            zoom: zoom,
+            centro: centro,
+            bounds: bounds,
             xyz: {
                 x: centro.lng,
                 y: centro.lat,
-                z: zoom
-            }
+                z: zoom,
+            },
         };
-        let userInput = prompt("Digite as coordenadas em ordem 'latitude longitude'\n\nSepare as coordenadas por espaço.\n\nEx:\n-23.123456 -46.123456");
+        let userInput = prompt(
+            "Digite as coordenadas em ordem 'latitude longitude'\n\nSepare as coordenadas por espaço.\n\nEx:\n-23.123456 -46.123456"
+        );
         let userInputText = userInput.split(" ");
         let userInputLat = parseFloat(userInputText[0]);
         let userInputLng = parseFloat(userInputText[1]);
         let markerIcon = L.icon({
-            iconUrl: '/maps/icons/marker-icon.png',
-            shadowUrl: '/maps/icons/marker-shadow.png',
+            iconUrl: "/maps/icons/marker-icon.png",
+            shadowUrl: "/maps/icons/marker-shadow.png",
             iconSize: [25, 41],
             iconAnchor: [12, 41],
             popupAnchor: [1, -34],
         });
-        let marker = L.marker([userInputLat, userInputLng], {icon : markerIcon}).addTo(mapa);
+        let marker = L.marker([userInputLat, userInputLng], {
+            icon: markerIcon,
+        }).addTo(mapa);
         mapa.setView([userInputLat, userInputLng], 15);
         return informacoes;
     };
@@ -116,5 +140,5 @@ export {
     FuncoesZoom,
     AdicionaAttribution,
     CriaMenuContexto,
-    FuncaoMapaInformacoes
-}
+    FuncaoMapaInformacoes,
+};
