6c4bb23670d27bf97ffd32364c82afafd2ff155b
Matheus Ryuki <matheuskaiya2@gmail.com>
2024-03-12 20:26:18 -0300

    Melhorias de estilização no botão de temperatura e precipitação

--- resources/js/Pages/Grafico/Sections/Grafico.vue
+++ resources/js/Pages/Grafico/Sections/Grafico.vue
@@ -78,8 +78,7 @@ function toggleVisibility(datasetLabel) {
             >
                 Temperatura
             </button>
-            <Link :href="route('mapa')" class="navigate-btn"
-                >Ir para o Mapa
+            <Link :href="route('mapa')" class="navigate-btn">
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     height="20"
@@ -89,8 +88,10 @@ function toggleVisibility(datasetLabel) {
                     <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                     <path
                         d="M384 476.1L192 421.2V35.9L384 90.8V476.1zm32-1.2V88.4L543.1 37.5c15.8-6.3 32.9 5.3 32.9 22.3V394.6c0 9.8-6 18.6-15.1 22.3L416 474.8zM15.1 95.1L160 37.2V423.6L32.9 474.5C17.1 480.8 0 469.2 0 452.2V117.4c0-9.8 6-18.6 15.1-22.3z"
-                    /></svg
-            ></Link>
+                    />
+                </svg>
+                Ir para o Mapa
+            </Link>
         </div>
     </div>
 </template>
